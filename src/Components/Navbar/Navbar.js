08531936caa075092dbfb2c7356356092b5c9641
avi4s <avisharma410@gmail.com>
2023-12-05 17:40:02 +0530

    initial commit

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import ModalCart from "@/pages/ModalCart/ModalCart";
 import { FaShoppingCart } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
@@ -7,6 +8,11 @@ import { useSelector } from "react-redux";
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const cartItems = useSelector((state) => state.cart_data.items);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -17,39 +23,54 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+      <nav className="bg-gray-800">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
             <Link href="/">
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
                 Ecommerce
               </span>
             </Link>
           </div>
 
-          <div
-            className="hidden w-full md:block md:w-auto"
-            id="navbar-solid-bg"
-          >
-            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
+          <div className="md:hidden">
+            <GiHamburgerMenu
+              onClick={toggleMenu}
+              className="text-white cursor-pointer"
+            />
+          </div>
 
-              <div className="flex">
-                <FaShoppingCart onClick={openModal} className="mt-1" />
-                <span className="ml-1">{cartItems?.length}</span>
-              </div>
-            </ul>
+          <div className="hidden md:flex space-x-4">
+            <Link href="/" className="text-white">
+              Home
+            </Link>
+            <div className="flex">
+              <FaShoppingCart onClick={openModal} className="mt-1 text-white" />
+              <span className="ml-1 text-white">{cartItems?.length}</span>
+            </div>
           </div>
         </div>
       </nav>
+      {menuOpen && (
+        <div className=" flex items-center justify-center z-50 ">
+          <div className="bg-black bg-opacity-50 " onClick={closeModal}></div>
+          <div className="fixed top-0 left-0 h-full bg-gray-800 w-36 p-4 transform translate-x-0">
+            <div className="flex flex-col mt-10">
+              <Link href="/" className="text-white">
+                Home
+              </Link>
+              <div className="flex">
+                <FaShoppingCart
+                  onClick={openModal}
+                  className="mt-1 text-white"
+                />
+                <span className="ml-1 text-white">{cartItems?.length}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
       {isModalOpen && (
         <div className="fixed inset-0 overflow-y-auto flex items-center justify-center z-50">
           <div
