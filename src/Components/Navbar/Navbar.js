6d2f559795a38d5c85a46fca357d4ea20ee7618d
avi4s <avisharma410@gmail.com>
2023-12-06 14:13:09 +0530

    navbar change

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,18 @@
 import ModalCart from "@/pages/ModalCart/ModalCart";
 import { FaShoppingCart } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const cartItems = useSelector((state) => state.cart_data.items);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -30,6 +35,26 @@ const Navbar = () => {
             <span className="ml-1 text-white">{cartItems?.length}</span>
           </div>
         </div>
+        <div className="sm:hidden">
+          <GiHamburgerMenu
+            onClick={toggleNavbar}
+            className="text-white cursor-pointer"
+          />
+        </div>
+        {isOpen && (
+          <div
+            className="fixed top-0 left-0 h-full w-36 p-4 transform translate-x-0"
+            style={{ background: "#94a3b8" }}
+          >
+            <Link href="/" className="block text-white">
+              Home
+            </Link>
+            <div className="flex">
+              <FaShoppingCart className="mt-1 text-white" onClick={openModal} />
+              <span className="ml-1 text-white">{cartItems?.length}</span>
+            </div>
+          </div>
+        )}
         {isModalOpen && (
           <div className="fixed inset-0 overflow-y-auto flex items-center justify-center z-50">
             <div
