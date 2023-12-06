e7988c3bbbff36cb68b56bd6d36fa4c5800171c8
avi4s <avisharma410@gmail.com>
2023-12-06 14:05:13 +0530

    navbar change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../Redux/Store";
+import "tailwindcss/tailwind.css";
 
 export default function App({ Component, pageProps }) {
   return (
